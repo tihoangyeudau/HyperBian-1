60db36db34cc53f0e862e1e0c5763b34134e0511
tihoangyeudau <tuantrung2905@gmail.com>
2021-12-16 03:51:13 +0700

    update

--- stage-hyperbian/00-install-hyperion/00-run.sh
+++ stage-hyperbian/00-install-hyperion/00-run.sh
@@ -1,32 +1,51 @@
 #!/bin/bash -e
 
+# Select the appropriate download path
+HYPERION_DOWNLOAD_URL="https://github.com/tihoangyeudau/hyperion.ng-12/releases/download"
+HYPERION_RELEASES_URL="https://api.github.com/repos/tihoangyeudau/hyperion.ng-12/releases"
+
+# Get the latest version
+HYPERION_LATEST_VERSION=$(curl -sL "$HYPERION_RELEASES_URL" | grep "tag_name" | head -1 | cut -d '"' -f 4)
+HYPERION_RELEASE=$HYPERION_DOWNLOAD_URL/$HYPERION_LATEST_VERSION/Ambilight-WiFi-$HYPERION_LATEST_VERSION-Linux-armv6l.deb
+
+# Download latest release
+echo 'Downloading Ambilight WiFi ........................'
+mkdir -p "$ROOTFS_DIR"/tmp
+curl -L $HYPERION_RELEASE --output "$ROOTFS_DIR"/tmp/ambilightwifi.deb
+
+# Download Rpi fan
+echo 'Downloading Rpi fan ........................'
+curl -sS -L --get https://github.com/tihoangyeudau/rpi-fan/releases/download/1.0.0/rpi-fan.tar.gz | tar --strip-components=1 -C ${ROOTFS_DIR}/usr/share/rpi-fan share/rpi-fan -xz
+
+# Copy service file
+cp rpi-fan.service ${ROOTFS_DIR}/etc/systemd/system/rpi-fan@.service
+
 # Enable SPI and force HDMI output
 sed -i "s/^#dtparam=spi=on.*/dtparam=spi=on/" ${ROOTFS_DIR}/boot/config.txt
 sed -i "s/^#hdmi_force_hotplug=1.*/hdmi_force_hotplug=1/" ${ROOTFS_DIR}/boot/config.txt
 
 # Modify /usr/lib/os-release
-sed -i "s/Raspbian/HyperBian/gI" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^NAME=.*$/NAME=\"HyperBian ${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/Raspbian/Rmlos/gI" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/^NAME=.*$/NAME=\"Rmlos ${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
 sed -i "s/^VERSION=.*$/VERSION=\"${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^HOME_URL=.*$/HOME_URL=\"https:\/\/hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^SUPPORT_URL=.*$/SUPPORT_URL=\"https:\/\/hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^BUG_REPORT_URL=.*$/BUG_REPORT_URL=\"https:\/\/hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/^HOME_URL=.*$/HOME_URL=\"https:\/\/rainbowmusicled.com\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/^SUPPORT_URL=.*$/SUPPORT_URL=\"https:\/\/rainbowmusicled.com\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/^BUG_REPORT_URL=.*$/BUG_REPORT_URL=\"https:\/\/rainbowmusicled.com\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
 
 # Custom motd
 rm "${ROOTFS_DIR}"/etc/motd
 rm "${ROOTFS_DIR}"/etc/update-motd.d/10-uname
-install -m 755 files/motd-hyperbian "${ROOTFS_DIR}"/etc/update-motd.d/10-hyperbian
+install -m 755 files/motd-rmlos "${ROOTFS_DIR}"/etc/update-motd.d/10-rmlos
 
 # Remove the "last login" information
 sed -i "s/^#PrintLastLog yes.*/PrintLastLog no/" ${ROOTFS_DIR}/etc/ssh/sshd_config
 
 on_chroot << EOF
-echo '---> Import the public GPG key from the APT server into HyperBian'
-wget -qO- https://apt.hyperion-project.org/hyperion.pub.key | gpg --dearmor -o /usr/share/keyrings/hyperion.pub.gpg
-echo '---> Add Hyperion to the APT sources'
-echo "deb [signed-by=/usr/share/keyrings/hyperion.pub.gpg] https://apt.hyperion-project.org/ stable main" | sudo tee /etc/apt/sources.list.d/hyperion.list
-echo '---> Update the APT sources and installing Hyperion'
-apt-get update && apt-get -y install hyperion
-echo 'Registering Hyperion'
-systemctl -q enable hyperion"@pi".service
+echo 'Installing Hyperion ........................'
+apt install /tmp/ambilightwifi.deb
+rm /tmp/ambilightwifi.deb
+mv /etc/systemd/system/ambilightwifid@.service /etc/systemd/system/ambilightwifi@.service
+echo 'Registering Ambilight WiFi & Rpi fan'
+systemctl -q enable ambilightwifi"@rml".service
+systemctl -q enable rpi-fan"@rml".service
 EOF
